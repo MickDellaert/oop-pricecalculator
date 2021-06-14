331e6628e8b7f4bc8a89fde3e871a7e6683f7a77
Your Nameeli <elighidiu@yahoo.com>
2021-06-14 11:46:38 +0200

    db connection

---
+++ Model/dbConnection.php
@@ -0,0 +1,20 @@
+<?php
+
+    function openConnection(): PDO
+    {
+        // No bugs in this function, just use the right credentials.
+        $dbhost = "localhost";
+        $dbuser = "root";
+        $dbpass = "becode";
+        $db = "products";
+
+        $driverOptions = [
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ];
+
+        return new PDO('mysql:host=' . $dbhost . ';dbname=' . $db, $dbuser, $dbpass, $driverOptions);
+    }
+
+}
