3a967bb11b7f587b18d3c429bc3dd745cef8df56
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2021-06-15 15:53:11 +0200

    homepage update

--- View/homepage.php
+++ View/homepage.php
@@ -3,9 +3,15 @@
 Anything complex should be calculated in the model -->
 <section>
 
-<?php foreach ($products AS $product) {
-    echo $product->getName();
-};?>
+    <form action="" method="post">
+        <select class="form-select" aria-label="Default select example">
+            <?php foreach ($products AS $product) {
+                echo '<option value="' . $product->getName() . '">' . '</option>';
+            };?>
+    </select>
+    </form>
+
+
 
 <!--    --><?php //echo $product->getID() ?>
 <!--    --><?php //echo $product->getPrice() ?>
@@ -15,4 +21,4 @@ Anything complex should be calculated in the model -->
     <p>Put your content here.</p>
 </section>
 
-<?php require 'includes/footer.php'?>
+<?php require 'includes/footer.php' ?>
